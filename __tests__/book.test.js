e9fcb87194c44029dd09fb6986b583e53205da7e
AminaOti <45714846+AminaOti@users.noreply.github.com>
2021-07-19 13:21:23 +0100

    Update book.test.js

--- __tests__/book.test.js
+++ __tests__/book.test.js
@@ -6,17 +6,6 @@ const { navigateToWebsite,
   selectDay,
   selectActivity, } = require("../helpers/testHelper");
 
-
-describe('book courts', () => {
-  const courtIds = {
-    court1Id: 'c2acbb00-af02-460d-b6db-444c2f617a80',
-    court2Id: '5fed3659-9c46-44c8-bd9d-1d0061e522d6'
-  }
-
-  function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
   it('navigate to ' + process.env.WEBSITE + 'and accept cookies', async () => {
     await navigateToWebsite()
     await acceptCookies()
